a94167a505d2c7d052d8992d45acf1b510a33602
Ame_x Edam <edame8080@gmail.com>
2024-06-18 05:47:55 +0000

    wip

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.get("/status", async (c) => {
             "User-Agent": c.req.header("User-Agent") || "FILE-UPLOADER-API:NO-USER-AGENT",
         },
         // @ts-ignore
-        proxy: "..."
+        proxy: "...",
     });
 
     if (err) {
